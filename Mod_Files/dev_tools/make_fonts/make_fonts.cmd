2a99188d52ae5ecab7de8054d2bd946060c7901f
FograinDaNoob@gmail.com <FograinDaNoob@gmail.com@cd7bd8fe-a22b-0e82-0650-8346824a0664>
2014-08-18 16:04:49 +0000

    - New version of "make_fonts" application, thx to: Vit_amiN - Fonts for Portuguese and Turkish translations

--- Mod_Files/dev_tools/make_fonts/make_fonts.cmd
+++ Mod_Files/dev_tools/make_fonts/make_fonts.cmd
@@ -1,13 +1,13 @@
 @setlocal enableextensions
 @echo off
 
-rem ===========================================================================
+rem ===================================================================================
 
 set "Curr_Path=%~dp0"
 set "Curr_Path=%Curr_Path:~0,-1%"
 set "PATH=%Curr_Path%;%PATH%"
 
-rem ===========================================================================
+rem ===================================================================================
 
 set "Fonts_Lst=%Curr_Path%\%~n0.txt"
 if not exist "%Fonts_Lst%" (goto :Quit_Script)
@@ -20,36 +20,38 @@ if not "%~1" == "" (
 )
 set "Fonts_Dir=%Game_Path%\gfx\vgui\fonts"
 
-rem ===========================================================================
+rem ===================================================================================
 
 echo.>>"%Fonts_Log%"
 echo>>"%Fonts_Log%" %date% [%time%] - Started...
 echo.>>"%Fonts_Log%"
 
-for /F "eol=; delims=	 tokens=1-4,* usebackq" %%a in ("%Fonts_Lst%") do (
+for /F "eol=; delims=	 tokens=1-5,* usebackq" %%a in ("%Fonts_Lst%") do (
 	set "File_Name=%Fonts_Dir%\%%~a.tga"
 	set "Font_Name=%%~b"
 	set "Font_Style=%%~c"
 	set "Font_Size=%%~d"
-	call :MakeTGAFont "File_Name" "Font_Name" "Font_Style" "Font_Size"
+	set "Font_Chrs=%%~e"
+	call :MakeTGAFont "File_Name" "Font_Name" "Font_Style" "Font_Size" "Font_Chrs"
 )>>"%Fonts_Log%"
 
 echo>>"%Fonts_Log%" %date% [%time%] - Finished.
 echo.>>"%Fonts_Log%"
 
-rem ===========================================================================
+rem ===================================================================================
 
 :Quit_Script
 
 endlocal
 exit /B 0
 
-rem ===========================================================================
+rem ===================================================================================
 
 :MakeTGAFont
 
 setlocal enableextensions
 
+if "%~5" == "" (endlocal & exit /B 1) else (call set "Font_Chrs=%%%~5%%" )
 if "%~4" == "" (endlocal & exit /B 1) else (call set "Font_Size=%%%~4%%" )
 if "%~3" == "" (endlocal & exit /B 1) else (call set "Font_Style=%%%~3%%")
 if "%~2" == "" (endlocal & exit /B 1) else (call set "Font_Name=%%%~2%%" )
@@ -75,7 +77,7 @@ for %%a in (%Font_Style%) do (
 	if /I "%%~a" == "Strikeout"	(set "FontStyle_S=true"))))
 )
 
-set "Cmd_Line=-font "%Font_Name%" -pointsize %Font_Size%"
+set "Cmd_Line=-font "%Font_Name%" -pointsize %Font_Size% -charset "%Font_Chrs%""
 if /I "%FontStyle_B%" == "true" (set "Cmd_Line=%Cmd_Line% -bold"     )
 if /I "%FontStyle_I%" == "true" (set "Cmd_Line=%Cmd_Line% -italic"   )
 if /I "%FontStyle_U%" == "true" (set "Cmd_Line=%Cmd_Line% -underline")
@@ -86,4 +88,4 @@ echo.
 
 endlocal & exit /B 0
 
-rem ===========================================================================
+rem ===================================================================================
